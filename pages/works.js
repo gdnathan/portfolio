5fa58a56f0823f0972690ae2db16bda96b3c2a1b
Nathan GD <nathan.gardet-derc@protonmail.ch>
2021-11-11 23:11:19 +0100

    adding bluestone work page

--- pages/works.js
+++ pages/works.js
@@ -9,6 +9,7 @@ import Section from '../components/section'
 import { WorkGridItem } from '../components/grid-item'
 import Layout from "../components/layouts/article"
 import thumbTerrare from '../public/images/work/Terrare.png'
+import thumbBlueStone from '../public/images/work/Bluestone.png'
 
 const Works = () => {
   return (
@@ -22,6 +23,11 @@ const Works = () => {
           <Section>
             <WorkGridItem id="terrare" title="Terrare" thumbnail={thumbTerrare}>
               Une marketplace NFTs mettant en relation des investisseurs, des organisations environnementales et des artistes engagés.
+            </WorkGridItem>
+          </Section>
+          <Section>
+            <WorkGridItem id="bluestone" title="BlueStone" thumbnail={thumbBlueStone}>
+              Un site de présentation de BlueStone, une association de deux freelances web et marketing
             </WorkGridItem>
           </Section>
         </SimpleGrid>
---
+++ pages/works/bluestone.js
@@ -0,0 +1,42 @@
+import { Container, Badge, Link, List, ListItem } from '@chakra-ui/react'
+import { ExternalLinkIcon } from '@chakra-ui/icons'
+import { Title, WorkImage, Meta } from '../../components/work'
+import P from '../../components/paragraph'
+import Layout from '../../components/layouts/article'
+
+const Work = () => {
+
+  return (
+    <Layout title="BlueStone">
+      <Container maxW="xl">
+        <Title>
+          BlueStone <Badge>2021</Badge>
+        </Title>
+        <P>
+            Une landing page présentant BlueStone, une collaboration de deux freelance, dont un web (moi), et une
+            en marketing.
+      <br />
+      <br />
+      C'est à travers ce site que nos clients entrent en premier contact avec nous, et découvrent nos offres.
+        </P>
+        <List ml={4} my={4}>
+          <ListItem>
+            <Meta>Type</Meta>
+            <span>Landing page</span>
+          </ListItem>
+          <ListItem>
+            <Meta>Site web</Meta>
+            <Link href="https://bluestone.vercel.app" target="_blank">bluestone.vercel.app<ExternalLinkIcon /></Link>
+          </ListItem>
+          <ListItem>
+            <Meta>Stack</Meta>
+            <span>React.js - Next.js</span>
+          </ListItem>
+        </List>
+      </Container>
+    </Layout>
+  )
+}
+
+export default Work
+
